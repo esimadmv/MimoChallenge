e6d2a05aefac1991b79d6980bb34dec3f43bdc9a
ehsanm4tqileap <ehsan.vazifeh@qileap.com>
2017-09-23 15:40:25 -0700

    user object

--- MimoiOSCodingChallenge/User.swift
+++ MimoiOSCodingChallenge/User.swift
@@ -10,17 +10,29 @@ import Foundation
 
 
 class User {
-    static let sharedInstance = User()
-    
+    static let user = User()
+    static let userDefaults = UserDefaults.standard
+
     var id: String!
     var accessToken: String!
     var emailaddress: String!
 
-    
     func initialize(id: String, access: String, email: String) {
         self.id = id
         self.accessToken = access
         self.emailaddress = email
     }
+    
+    class func currentUser() -> User? {
+        
+        guard let id = userDefaults.string(forKey: "id"), let access = userDefaults.string(forKey: "accesstoken"),
+            let email = userDefaults.string(forKey: "email") else {
+                return nil
+        }
+        User.user.initialize(id: id, access: access, email: email)
+        return User.user
+    }
+    
+    
     
 }
